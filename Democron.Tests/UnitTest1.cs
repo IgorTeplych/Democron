b2fe13b240c80dbc2bd271b7f67d89ff588f9393
Igor Teplych <igorvrt@mail.ru>
2023-01-14 11:58:37 +0600

    Добавил тестовый проект

---
+++ Democron.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Democron.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestGrafFromWebinar()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+}
